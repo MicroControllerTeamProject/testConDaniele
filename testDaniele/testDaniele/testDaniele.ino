41fc70245749b80031458713a30fd5eb2b3a85f1
Luigi Santagada <luigi.santagada@live.com>
2023-02-28 18:15:20 +0100

    .

---
+++ testDaniele/testDaniele/testDaniele.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		testDaniele.ino
+ Created:	2/28/2023 2:02:32 PM
+ Author:	luigi.santagada
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
