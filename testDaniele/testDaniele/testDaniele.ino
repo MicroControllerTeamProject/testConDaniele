ce3e595137bd85f4b2ccb58a6c51ea5703e0cdcf
luigisantagada <luigi.santagada@live.com>
2023-03-01 10:34:48 +0100

    .

--- testDaniele/testDaniele/testDaniele.ino
+++ testDaniele/testDaniele/testDaniele.ino
@@ -1,8 +1,8 @@
 /*
  Name:		testDaniele.ino
  Created:	2/28/2023 2:02:32 PM
- Author:	luigi.santagada
-*/
+ Author:	luigi.santagad
+ //lsgsoftware modified
 
 // the setup function runs once when you press reset or power the board
 void setup() {
