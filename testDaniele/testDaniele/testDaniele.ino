10dd8d92e431fe022ef946d8acfbb53df659b00b
luigisantagada <luigi.santagada@live.com>
2023-03-01 13:03:58 +0100

    .

--- testDaniele/testDaniele/testDaniele.ino
+++ testDaniele/testDaniele/testDaniele.ino
@@ -6,7 +6,7 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-	Serial.begin(19200);
+	Serial.begin(1920);
 }
 
 // the loop function runs over and over again until power down or reset
