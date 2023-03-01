57aced9a185aa5a65951f8aca1e9d6916eb617f6
Lsg Software <lsgsoftware.dev01@hotmail.com>
2023-03-01 13:19:33 +0100

    Merge branch 'main' of https://github.com/MicroControllerTeamProject/testConDaniele into main
    # Conflicts:
    #	testDaniele/testDaniele/testDaniele.ino

--- testDaniele/testDaniele/testDaniele.ino
+++ testDaniele/testDaniele/testDaniele.ino
@@ -6,11 +6,8 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-<<<<<<< HEAD
 	Serial.begin(19200);
-=======
 	int a = 10;
->>>>>>> 421c485f9d73853d82b0d81a7049cf0cc22e508e
 }
 
 // the loop function runs over and over again until power down or reset
